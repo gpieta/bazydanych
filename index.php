5916961c384c331174bb15d154b7e6f5075bf0ed
Gabriela Pięta <gpieta@zssalezjanie.com>
2017-05-24 12:07:47 +0200

    połączenie z bazą danych

--- index.php
+++ index.php
@@ -1,4 +1,10 @@
 <?php
+$dbhost = 'localhost';
+$dbuser = 'root';
+$dbpass = '';
+$dbname = 'Articles';
 
+$connect = mysql_connect($dbhost,$dbuser,$dbpass);
+mysql_select_db($dbname,$connect);
 
 ?>
