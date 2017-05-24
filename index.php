8d4ccfef816d02b57fd056825a6fb2039fe2a3bd
Gabriela Pięta <gpieta@zssalezjanie.com>
2017-05-24 12:09:39 +0200

    formularz

--- index.php
+++ index.php
@@ -8,3 +8,10 @@ $connect = mysql_connect($dbhost,$dbuser,$dbpass);
 mysql_select_db($dbname,$connect);
 
 ?>
+
+
+<form action="GET">
+    <input type="text" name='author'>
+    <input type="text" name='content'>
+    <input type="submit" value='dodaj do bazy danych'>
+</form>
