bc3a849b40112f0754dd3364b799d891d03e7287
Gabriela Pięta <gpieta@zssalezjanie.com>
2017-06-06 11:59:21 +0200

    wszystko zrobione

--- index.php
+++ index.php
@@ -7,6 +7,15 @@ $dbname = 'Articles';
 $connect = mysql_connect($dbhost,$dbuser,$dbpass);
 mysql_select_db($dbname,$connect);
 
+
+
+if(isset($_GET['author']) and isset($_GET['content']))
+{
+    $author = $_GET['author'];
+    $content = $_GET ['content'];
+
+    mysql_query("INSERT INTO Article VALUES (null,'$author', '$content')");
+}
 ?>
 
 
